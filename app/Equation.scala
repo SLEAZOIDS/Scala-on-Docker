78831f3bba8af44c1a8776cb14c97add5efaf5f4
Mac_Anje <yoshioka@accs-c.co.jp>
2017-11-19 00:01:58 +0900

    連立方程式の計算

--- app/Equation.scala
+++ app/Equation.scala
@@ -1,9 +1,31 @@
 import breeze.linalg._
 import breeze.numerics._
 
+/* ax+by=c, dx+ey=f の二元連立一次方程式を計算する
+  args a b c d e f
+*/
+
 object Equation{
   def main(args: Array[String]){
-    val m1 = DenseMatrix((1,2), (3,4))
-    println(m1)
+    val a = args(0).toDouble
+    val b = args(1).toDouble
+    val c = args(2).toDouble
+    val d = args(3).toDouble
+    val e = args(4).toDouble
+    val f = args(5).toDouble
+
+    val ad = DenseVector(a, d)
+    val be = DenseVector(b, e)
+    val cf = DenseVector(c, f)
+    val (x, y) = solve(ad, be, cf)
+    val rx = round(x)
+    val ry = round(y)
+    println(s"x = $rx  y = $ry")
+  }
+
+  def solve(ad: DenseVector[Double], be: DenseVector[Double], cf: DenseVector[Double]): (Double, Double) = {
+      val x = det(DenseMatrix(cf, be)) / det(DenseMatrix(ad, be))
+      val y = det(DenseMatrix(ad, cf)) / det(DenseMatrix(ad, be))
+      (x, y)
   }
 }
