915eba618d010cf666ad01b133a892656805198b
SLEAZOIDS <yoshioka@accs-c.co.jp>
2017-11-19 15:11:20 +0900

    標準入力から連立方程式の生成

--- app/Equation.scala
+++ app/Equation.scala
@@ -1,18 +1,36 @@
 import breeze.linalg._
 import breeze.numerics._
 
-/* ax+by=c, dx+ey=f の二元連立一次方程式を計算する
-  args a b c d e f
+/*
+  ax+by=c, dx+ey=f の二元連立一次方程式を計算する
+
+  exp:
+  input: 1 2 3 4 5 6
+  →
+  x + 2y = 3
+  4x + 5y = 6
+  (x = -1, y =2)
 */
 
 object Equation{
   def main(args: Array[String]){
-    val a = args(0).toDouble
-    val b = args(1).toDouble
-    val c = args(2).toDouble
-    val d = args(3).toDouble
-    val e = args(4).toDouble
-    val f = args(5).toDouble
+    println("ax+by=c, dx+ey=f の二元連立一次方程式を計算します...")
+    println("必要な変数をaから半角スペース区切りの数値で入力してください")
+    println("exp:1 2 4 5 -7 9")
+
+    var input: Array[Int] = Array.empty
+    input = readLine.split(" ").map(_.toInt)
+    while(input.length != 6) {
+      println("入力する数値は6つにしてください")
+      input = readLine.split(" ").map(_.toInt)
+    }
+
+    val a = input(0).toDouble
+    val b = input(1).toDouble
+    val c = input(2).toDouble
+    val d = input(3).toDouble
+    val e = input(4).toDouble
+    val f = input(5).toDouble
 
     val ad = DenseVector(a, d)
     val be = DenseVector(b, e)
