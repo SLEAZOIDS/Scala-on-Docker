9d9745e094434f17fd86b502cf8c186c32db354e
SLEAZOIDS <yoshioka@accs-c.co.jp>
2017-11-19 15:34:11 +0900

    配列調査の方も標準入力からデータ取得

--- app/Equation2.scala
+++ app/Equation2.scala
@@ -7,21 +7,35 @@ import scala.util.control.Breaks.{break, breakable}
 */
 /* 
   exp:
-  args: 12 1 2 3 4 5 6 9
+  input: 12 1 2 3 4 5 6 9
   K = 12 
   A = Array(1, 2, 3, 4, 5, 6, 9)
 */
 
 object Equation2{
   def main(args: Array[String]){
-    val K = args(0).toInt
-    var A: Array[Int] = Array.empty
-    
-    for ((number, index) <- args.zipWithIndex) {
-      if (index != 0)
-        A = A :+ number.toInt
+    println("A[i] + A[j] == K を満たす i<j の存在判定を行います...")
+    println("整数K入力してください")
+    println("exp:12")
+
+    var input: Array[Int] = Array.empty
+    input = readLine.split(" ").map(_.toInt)
+    while(input.length != 1) {
+      println("入力する数値は1つにしてください")
+      input = readLine.split(" ").map(_.toInt)
     }
-    
+
+    val K = input(0).toInt
+
+    println("整数配列Aを半角スペース区切りで入力してください")
+    println("exp:1 2 3 4 5 6 9")
+    var A: Array[Int] = Array.empty
+    A = readLine.split(" ").map(_.toInt)
+    while(A.length <= 1) {
+      println("整数配列には2つ以上の数値を設定してください")
+      A = readLine.split(" ").map(_.toInt)
+    }
+
     //以下は必ずcombinations(0)<combinations(1)の組み合わせとなる
     val combinations = A.combinations(2).toList
 
